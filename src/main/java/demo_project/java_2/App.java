8c738cc4b1c299803b0996df111f2b692d12b8f1
lohithak28 <lohitha.kunduru28@gmail.com>
2025-11-24 19:32:38 +0530

    new

--- src/main/java/demo_project/java_2/App.java
+++ src/main/java/demo_project/java_2/App.java
@@ -6,5 +6,6 @@ package demo_project.java_2;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hey");
     }
 }
